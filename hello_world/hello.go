4fdecf173ce6a1f508ab446bbd0daedac0fba240
Odhran McGranaghan <odhran.mcgranaghan@dailypay.com>
2024-06-17 11:15:37 +0100

    refactoring

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,15 +2,43 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(name string) string {
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "spanish"))
 }
+
+// Notes:
+// In our function signature we have made a named return value (prefix string).
+// This will create a variable called prefix in your function.
+// It will be assigned the "zero" value. This depends on the type, for example ints are 0 and for strings it is "".
+// You can return whatever it's set to by just calling return rather than return prefix.
+// This will display in the Go Doc for your function so it can make the intent of your code clearer.
+// default in the switch case will be branched to if none of the other case statements match.
+// The function name starts with a lowercase letter. In Go, public functions start with a capital letter, and private ones start with a lowercase letter. We don't want the internals of our algorithm exposed to the world, so we made this function private.
+// Also, we can group constants in a block instead of declaring them on their own line. For readability, it's a good idea to use a line between sets of related constants.
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -4,13 +4,23 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying hello when an empty string is provided", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Remy", "French")
+		want := "Bonjour, Remy"
 		assertCorrectMessage(t, got, want)
 	})
 }
