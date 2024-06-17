f41c122ee8552e1fcebc6dfba9a202517adf187d
Odhran McGranaghan <odhran.mcgranaghan@dailypay.com>
2024-06-17 11:04:01 +0100

    refactor tests and hello_world function

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,11 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello when an empty string is provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
 
