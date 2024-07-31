80d209133651515cb642d1e195e98128befe1972
Odhran McGranaghan <odhran.mcgranaghan@dailypay.com>
2024-07-31 16:41:52 +0100

    commit fdump from last week

---
+++ acceptancetests/playground.go
@@ -0,0 +1,23 @@
+package acceptance_testing
+
+import (
+	"context"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var (
+		ctx        = context.Background()
+		httpServer = &http.Server{Addr: ":8080", Handler: http.HandlerFunc(acceptancetests.SlowHandler)}
+		server     = gracefulshutdown.NewServer(httpServer)
+	)
+
+	if err := server.ListenAndServe(ctx); err != nil {
+		// this will typically happen if our responses aren't written before the ctx deadline, not much can be done
+		log.Fatalf("uh oh, didn't shutdown gracefully, some responses may have been lost %v", err)
+	}
+
+	// hopefully, you'll always see this instead
+	log.Println("shutdown gracefully! all responses were sent")
+}
